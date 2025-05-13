b14f3967baec348dd7795fcce866a6c00be904a5
yanni13 <iumd6075@gmail.com>
2025-05-13 12:49:34 +0900

    docs: PR 템플릿 추가

---
+++ github-semiar/github-semiar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github-semiar
+//
+//  Created by 아우신얀 on 5/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github-semiar/github-semiar/github_semiarApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_semiarApp.swift
+//  github-semiar
+//
+//  Created by 아우신얀 on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct github_semiarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
